2b9607e24dd6bf9cd05cff55f82f7011aae7a87b
Bala <balakrishnan.ganesan@rci.rogers.com>
2021-05-11 09:23:44 -0400

    Shared module for dashboard

---
+++ src/app/shared/dashboard/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../state/app.state';
+import { selectBookCollection, selectBooks } from '../../state/books.selectors';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnInit {
+  public books$: Observable<any>;
+  public bookCollection$: Observable<any>;
+  public appState: any;
+  public formattedData = '';
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.books$ = this.store.pipe(
+      select(selectBooks)
+    );
+
+    this.store.subscribe((data) => {
+      this.appState = data;
+      this.formattedData = JSON.stringify(this.appState, undefined, 4);
+    });
+    this.bookCollection$ = this.store.pipe(select(selectBookCollection));
+  }
+}
